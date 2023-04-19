0c5527fbc273b2d88c03080dc7d69498e7535ecd
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-20 00:26:11 +0100

    Navbar section started

---
+++ components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#0e0e10] mb-[2px] z-10'>
+        {/* Left Side */}
+        <div className='flex grow items-center justify-start'>
+
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
