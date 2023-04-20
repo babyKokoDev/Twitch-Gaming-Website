6907a3ac59387e900a8b126760439bd0adaa09f3
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-20 09:05:20 +0100

    Navbar section started

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,11 +1,105 @@
-import React from 'react'
+import Link from 'next/link'
+import React, { Fragment } from 'react'
+import Logo from '../public/assets/logo.png'
+import Image from 'next/image'
+import { Menu, Transition } from '@headlessui/react'
+import {BsThreeDotsVertical, BsSearch} from 'react-icons/bs'
+
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 const NavBar = () => {
   return (
     <div className='fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#0e0e10] mb-[2px] z-10'>
         {/* Left Side */}
         <div className='flex grow items-center justify-start'>
+         <Link href = "/">
+                <Image src={Logo} width={36} height={36} className='cursor-pointer z-10' />
+         </Link>
+         <p className='p-4'>Browse</p>
+         <div className='p-4'>
+         <Menu as='div' className='relative text-left'>
+            <div className='flex'>
+              <Menu.Button>
+                <BsThreeDotsVertical size={20} />
+              </Menu.Button>
+            </div>
 
+            <Transition
+              as={Fragment}
+              enter='transition ease-out duration-100'
+              enterFrom='transform opacity-0 scale-95'
+              enterTo='transform opacity-100 scale-100'
+              leave='transition ease-in duration-75'
+              leaveFrom='transform opacity-100 scale-100'
+              leaveTo='transform opacity-0 scale-95'
+            >
+              <Menu.Items className='origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-[#0e0e10] ring-1 ring-white ring-opacity-5 focus:outline-none'>
+                <div className='py-1'>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href='#'
+                        className={classNames(
+                          active
+                            ? 'bg-gray-500 text-gray-100'
+                            : 'text-gray-200',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                        Settings
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href='#'
+                        className={classNames(
+                          active
+                            ? 'bg-gray-500 text-gray-100'
+                            : 'text-gray-200',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                        Support
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href='#'
+                        className={classNames(
+                          active
+                            ? 'bg-gray-500 text-gray-100'
+                            : 'text-gray-200',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                        License
+                      </a>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+         </div>
+        </div>
+
+        {/* Middle */}
+        <div className='hidden md:flex grow-[2] items-center justify-center'>
+            <div className='bg-gray-500 flex justify-center items-center max-w-[400px] m-auto p-2 rounded-2xl'>
+                <div>
+                    <input type="text" className='bg-transparent border-none text-white focus:outline-none' />
+                </div>
+                <div>
+                    <BsSearch />
+                </div>
+            </div>
         </div>
     </div>
   )
