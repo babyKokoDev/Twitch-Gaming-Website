b7d75c4b5abd54e00cd90727b681509c5748686f
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-22 00:20:40 +0100

    sideMenu bar in progress

--- components/SideMenu.jsx
+++ components/SideMenu.jsx
@@ -1,38 +1,62 @@
 import React from 'react'
 import { rec_channels, top_channels } from '@/data/mock-data'
-import {RiMovieLine} from 'react-icons/ri'
+import { RiMovieLine } from 'react-icons/ri'
 import Image from 'next/image'
-import {BsDot} from 'react-icons/bs'
+import { BsDot } from 'react-icons/bs'
+import { Scrollbar } from 'react-scrollbars-custom'
 
 
 const SideMenu = () => {
-  return (
-    <div className='fixed w-14 sm:w-16 xl:w-[15rem] h-screen p-2 bg-[#0e0e10]'>
-          <div>
-            <p className='hidden xl:flex uppercase font-bold text-sm pb-4'>Recommended Channels</p>
-            <p><RiMovieLine size={20} className='xl:hidden justify-center w-full' /></p>
-          </div>
-          <div>
+    return (
+       
+        <div className='scrollbar-hide overflow-y-auto sm:hover:scrollbar-default fixed w-14 sm:w-16 xl:w-[15rem] h-screen   p-2 bg-[#0e0e10]'>
+            <div>
+                <p className='hidden xl:flex uppercase font-bold text-sm pb-4'>Recommended Channels</p>
+                <p><RiMovieLine size={20} className='xl:hidden justify-center w-full' /></p>
+            </div>
             {
-                rec_channels.map((item, index)=> (
-                   <div key={index} className='inline-flex items-center w-full py-[2px]'>
-                      <div>
-                        <Image src={item.avatar} width="50" height="50" alt={item.username} />
-                      </div>
-                      <div className='hidden xl:flex justify-between w-full ms-2'>
+                rec_channels.map((item, index) => (
+                    <div key={index} className='inline-flex items-center w-full py-[2px]'>
                         <div>
-                            <p>{item.username}</p>
-                            <p className='text-gray-400'>{item.game_name}</p>
+                            <Image src={item.avatar} width="50" height="50" alt={item.username} />
                         </div>
-                        <p className='flex items-center'><BsDot color='red' size={40} /></p>
-                      </div>
-                   </div>
+                        <div className='hidden xl:flex justify-between w-full ms-2'>
+                            <div>
+                                <p>{item.username}</p>
+                                <p className='text-gray-400'>{item.game_name}</p>
+                            </div>
+                            <p className='flex items-center'><BsDot color='red' size={40} />{item.rank}K</p>
+                        </div>
+                    </div>
 
                 ))
             }
-          </div>
-    </div>
-  )
+
+            <div>
+                <p className='hidden xl:flex uppercase font-bold text-sm py-4'>Top Channels</p>
+                <p><RiMovieLine size={20} className='xl:hidden justify-center w-full' /></p>
+            </div>
+
+            {
+                top_channels.map((item, index) => (
+                    <div key={index} className='inline-flex items-center w-full py-[2px]'>
+                      <div>
+                            <Image src={item.avatar} width="50" height="50" alt={item.username} />
+                        </div>
+                        <div className='hidden xl:flex justify-between w-full ms-2'>
+                            <div>
+                                <p>{item.username}</p>
+                                <p className='text-gray-400'>{item.game_name}</p>
+                            </div>
+                            <p className='flex items-center'><BsDot color='red' size={40} />{item.rank}K</p>
+                        </div>
+
+                    </div>
+            ))
+            }
+
+        </div>
+    )
 }
 
 export default SideMenu
