c98dc49ca05b1b8bbf8caa7af1392afb436de627
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-22 13:00:51 +0100

    hero section added

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SideMenu from './SideMenu'
 import LiveChannels from './LiveChannels'
+import Hero from './Hero'
 
 const MainPage = () => {
   return (
        <div className='absolute left-[64px] xl:left-[220px]'>
+        <Hero />
         <LiveChannels />
        </div>
   )
